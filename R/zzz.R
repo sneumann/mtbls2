f75d4b19afb410c4684d7688dc1cc0e0b02d50ef
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-10-06 17:25:58 +0000

    moving mtbls2 to data repos
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/mtbls2@3437 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+# save compressed to avoid warnings:
+# save(mtbls2Set, file="data/mtbls2.rda", compress='xz') 
+load(file.path(find.package("mtbls2"), "data", "mtbls2.rda"))
+
+# The following path fixup requires that the mtbls2.rda in BioC has the relative path
+attr(mtbls2Set, "filepaths") <- file.path(find.package("mtbls2"), gsub("/", .Platform$file.sep, attr(mtbls2Set, "filepaths")))
